2581a0b904cf4eb9ed4299e4f400c2bf1a5bc539
dunitian <1054186320@qq.com>
2016-08-31 10:23:39 +0800

    在用Result的时候，内部会调用Wait（这个时候Wait其实就可以省略了）

--- 2016-08-29/05.TaskB/Program.cs
+++ 2016-08-29/05.TaskB/Program.cs
@@ -4,14 +4,20 @@ using System.Threading.Tasks;
 
 namespace _05.TaskB
 {
+    /// <summary>
+    /// 在用Result的时候，内部会调用Wait（这个时候Wait其实就可以省略了）
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("主线程启动~~~{0}\n", Thread.CurrentThread.ManagedThreadId);
-            Task<int> task = new Task<int>(n => GetNum((int)n),null);
+            int num = 1000;
+            Console.WriteLine("计算前：{0}\n", num);
+            Task<int> task = new Task<int>(n => GetNum(Convert.ToInt32(n)), num);
             task.Start();
-            Thread.Sleep(500);
+            //task.Wait();//在一个线程调用Wait方法时，系统会检查线程要等待的Task是否已经开始执行，如果任务正在执行，那么这个Wait方法会使线程阻塞，知道Task运行结束为止
+            Console.WriteLine("计算后：{0}\n", task.Result);//在用Result的时候，内部会调用Wait
             Console.WriteLine("主线程结束~~~{0}\n", Thread.CurrentThread.ManagedThreadId);
             Console.Read();
         }
