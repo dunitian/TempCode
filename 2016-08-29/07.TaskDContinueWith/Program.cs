c6900f1eafb1d6f35ca899f35272690c762aaa61
dunitian <1054186320@qq.com>
2016-09-01 09:33:05 +0800

    AttachedToParent:指定将任务附加到任务层次结构中的某个父级

--- 2016-08-29/07.TaskDContinueWith/Program.cs
+++ 2016-08-29/07.TaskDContinueWith/Program.cs
@@ -8,22 +8,21 @@ namespace _07.TaskDContinueWith
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("主线程开始：{0}\n", Thread.CurrentContext.ContextID);
+            Console.WriteLine("主线程开始：{0}\n", Thread.CurrentThread.ManagedThreadId);
             int num = 2000;
             Console.WriteLine("计算之前：{0}\n", num);
             var task = new Task<int>(n => GetNum(Convert.ToInt32(n)), num);
             task.Start();
-
             //任务执行完毕后执行
-            task.ContinueWith(t => Console.WriteLine("计算之后：{0}。线程ID：{1}\n", t.Result, t.Id), TaskContinuationOptions.OnlyOnRanToCompletion);
+            task.ContinueWith(t => Console.WriteLine("计算之后：{0}。线程ID：{1}\n", t.Result, Task.CurrentId), TaskContinuationOptions.OnlyOnRanToCompletion);
             //发生异常的时候执行
-            task.ContinueWith(t => Console.WriteLine("异常信息：{0}。线程ID：{1}\n", t.Exception, t.Id), TaskContinuationOptions.OnlyOnFaulted);
+            task.ContinueWith(t => Console.WriteLine("异常信息：{0}。线程ID：{1}\n", t.Exception, Task.CurrentId), TaskContinuationOptions.OnlyOnFaulted);
             //任务取消的时候执行
-            task.ContinueWith(t => Console.WriteLine("任务已经取消，线程ID：{0}\n", t.Id),TaskContinuationOptions.OnlyOnCanceled);
+            task.ContinueWith(t => Console.WriteLine("任务已经取消，线程ID：{0}\n", Task.CurrentId),TaskContinuationOptions.OnlyOnCanceled);
 
             task.Wait();
 
-            Console.WriteLine("主线程结束：{0}\n", Thread.CurrentContext.ContextID);
+            Console.WriteLine("主线程结束：{0}\n", Thread.CurrentThread.ManagedThreadId);
             Console.Read();
         }
 
---
+++ 2016-08-29/08.TaskEAttachedToParent/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _08.TaskEAttachedToParent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("主线程开始：{0}\n", Thread.CurrentThread.ManagedThreadId);
+            Task<int[]> tasks = new Task<int[]>(() =>
+            {
+                var results = new int[3];
+
+                new Task(() => results[0] = GetNum(1), TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[1] = GetNum(2), TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => results[2] = GetNum(3), TaskCreationOptions.AttachedToParent).Start();
+
+                return results;
+
+            });
+            tasks.Start();
+
+            var task = tasks.ContinueWith(t => Array.ForEach(tasks.Result, n => Console.WriteLine("线程ID：{0},计算结果：{1}", Task.CurrentId, n)));
+
+            Console.WriteLine("主线程结束：{0}\n", Thread.CurrentThread.ManagedThreadId);
+            Console.Read();
+        }
+
+        private static int GetNum(int v)
+        {
+            Console.WriteLine("子线程在此：{0}\n", Task.CurrentId);
+            Thread.Sleep(1000);
+            Console.WriteLine("子线程走也：{0}\n", Task.CurrentId);
+            return v * 2;
+        }
+    }
+}
