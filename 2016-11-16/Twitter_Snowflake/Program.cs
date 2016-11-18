ed90e35fdf836ad8227bff6a02fe81b61877562d
dunitian <1054186320@qq.com>
2016-11-18 20:42:20 +0800

    雪花算法

---
+++ 2016-11-16/Twitter_Snowflake/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Twitter_Snowflake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"开始执行 {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffffff")}------{Snowflake.Instance().GetId()} \n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
