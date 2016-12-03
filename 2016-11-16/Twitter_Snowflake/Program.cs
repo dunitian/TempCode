179c372f1de3a0f1fc8ba228d081de5ecf61c2a5
dunitian <1054186320@qq.com>
2016-12-03 17:08:50 +0800

    多线程并发下还是会有重复的

--- 2016-11-16/Twitter_Snowflake/Program.cs
+++ 2016-11-16/Twitter_Snowflake/Program.cs
@@ -1,16 +1,58 @@
-﻿using System;
+﻿#region Test1
+//using System;
+
+//namespace Twitter_Snowflake
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (int i = 0; i < 1000; i++)
+//            {
+//                Console.WriteLine($"开始执行 {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffffff")}------{Snowflake.Instance().GetId()} \n");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//} 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Twitter_Snowflake
 {
     class Program
     {
+        private static List<long> list = new List<long>();
         static void Main(string[] args)
         {
-            for (int i = 0; i < 1000; i++)
-            {
-                Console.WriteLine($"开始执行 {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffffff")}------{Snowflake.Instance().GetId()} \n");
-            }
+            Task.Run(() => GetUid());
+            Task.Run(() => GetUid());
+            Task.Run(() => GetUid());
+            Console.WriteLine("稍等10s");
+            Task.Run(() => Printf());
             Console.ReadKey();
+        }
+
+        private static void GetUid()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var id = Snowflake.Instance().GetId();
+                //Console.WriteLine($"开始执行 {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffffff")}------{id} \n");
+                list.Add(id);
+            }
+        }
+
+        private static void Printf()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.Distinct().Count());
         }
     }
 }
+
