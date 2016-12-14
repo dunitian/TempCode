81995f843300b69e1c28bd5ecc8f87247fcb57a5
dunitian <1054186320@qq.com>
2016-12-14 12:01:45 +0800

    ？？ demo

---
+++ 2016-12-13/ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "0";var b = a ?? "g";
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
